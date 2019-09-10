f9c11fd568fbc3a7dd4c1559c30c57ab1c480f99
Veronica Isabel <veronica@Veronica-Isabel.local>
2019-09-10 08:18:38 -0300

    subiendo responsive

--- src/data.js
+++ src/data.js
@@ -1,33 +1,29 @@
 
-
-  //para filtrar por candy
-  const data = POKEMON.pokemon
-  window.filterData={
-   firstFilterCandy:(data, valueCandy) => {
-    const dataFilterCandy = data.filter(element => {
-    return element.candy_count == (valueCandy) 
-    
-     })
-
-    return dataFilterCandy
-    },
-//para filtrar por eggs
-    secondFilterEggs:(data, valueEgg) => {
-      const dataFilterEgg = data.filter(element => {
-      return element.egg == (valueEgg) 
-    
-       })
-    
-      return dataFilterEgg
-      },
-      //para filtrar por Type
-      thirdFilterType:(data,valueTypePokemon) =>{
-        const dataFilterPokemonType= data.filter(element =>{
-          return element.type.includes(valueTypePokemon) 
-        })
-        return dataFilterPokemonType;
-      }
-  }
-    
-
+//para filtrar por candy
   
+window.filterData={
+  firstFilterCandy:(data, valueCandy) => {
+   const dataFilterCandy = data.filter(element => {
+   return element.candy_count == (valueCandy) 
+   
+    })
+   
+   return dataFilterCandy
+   },
+//para filtrar por eggs
+   secondFilterEggs:(data, valueEgg) => {
+     const dataFilterEgg = data.filter(element => {
+     return element.egg == (valueEgg) 
+   
+      })
+   
+     return dataFilterEgg
+     },
+     //para filtrar por Type
+     thirdFilterType:(data,valueTypePokemon) =>{
+       const dataFilterPokemonType= data.filter(element =>{
+         return element.type.includes(valueTypePokemon) 
+       })
+       return dataFilterPokemonType;
+     }
+ }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 //Nav Menu Principal
 document.getElementById("button-menu1").addEventListener("click", () =>{
   document.getElementById("news-screen").style.display="block";
@@ -6,36 +5,38 @@ document.getElementById("button-menu1").addEventListener("click", () =>{
   document.getElementById("listSelect").style.display="none";
   document.getElementById("estadisticas-screen").style.display="none";
   });
-  
+
   document.getElementById("button-menu2").addEventListener("click", () =>{
   document.getElementById("news-screen").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("listSelect").style.display="block";
   document.getElementById("estadisticas-screen").style.display="none";
   });
-  
+
   document.getElementById("button-menu3").addEventListener("click", () =>{
   document.getElementById("news-screen").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("listSelect").style.display="none";
   document.getElementById("estadisticas-screen").style.display="block";
   });
-  
-  document.getElementById("imagen-logo").addEventListener("click", () =>{
-  document.getElementById("news-screen").style.display="none";
-  document.getElementById("home").style.display="block";
-  document.getElementById("listado-screen").style.display="none";
-  document.getElementById("estadisticas-screen").style.display="none";
-  });
-  
-  
+
+document.getElementById("imagen-logo").addEventListener("click", () =>{
+document.getElementById("news-screen").style.display="none";
+document.getElementById("home").style.display="block";
+document.getElementById("listado-screen").style.display="none";
+document.getElementById("estadisticas-screen").style.display="none";
+});
+
+
 const selects1 = document.getElementById("listSelect");
-const containerFilters = document.getElementById("button-menu2") 
+const containerFilters = document.getElementById("button-menu2")
 containerFilters.addEventListener("click", ()=>{
     selects1.style.display = "block";
 });
 
  //Funciones de filtrado
+const data = window.POKEMON.pokemon;
+
 
 const container = document.getElementById("showPokemones")
   const selectCandy  = document.getElementById("candy_count");
@@ -44,77 +45,67 @@ const container = document.getElementById("showPokemones")
 //let valueCandy = selectCandy.options[selectCandy.selectedIndex].value
 
  let prindCandy = window.filterData.firstFilterCandy(data, valueCandy)
- 
- 
+
+
 
 //                  // imprimir resultados
 document.getElementById("showPokemones").innerHTML="";
 prindCandy.forEach(element => {
-  container.innerHTML += `<div>
-
-                                <div class="cartPk">
+container.innerHTML += `<div>
+                          <div class="cartPk">
+                              <img class="imgPk" src = ${element.img}>
                                 <div class="cartPk1">
-                                <img src = ${element.img} class="imgPk">
-
-                                <p> ${element.name}</p>
-                                <p> # ${element.id}</p>
-                               </div>
+                                  <p id="pokeName"> ${element.name}</p>
+                                  <p id="pokedex"> # ${element.id}</p>
                                 </div>
-                                </div>`
-                            
-
-    })})
-
-
-    
-    const selectEgg  = document.getElementById("type_egg");
-    selectEgg.addEventListener("change", () =>{
-    let valueEgg = document.getElementById("type_egg").value    
-  //let valueCandy = selectCandy.options[selectCandy.selectedIndex].value
-   let prindEgg = window.filterData.secondFilterEggs(data, valueEgg)
-   
-
-  //                  // imprimir resultados egss
-  document.getElementById("showPokemones").innerHTML="";
-  prindEgg.forEach(element => {
-    container.innerHTML += `<div>
-                                  <div class="cartPk">
-                                  <div class="cartPk1">
-                                  <img src = ${element.img} class="imgPk"> 
-                           
-                                  <p> ${element.name}</p>
-                                  <p> # ${element.id}</p>
-                                 </div> 
-                                  </div>    
-                                  </div> 
-                              `
-      
-      })})
-
-    const selectTypePokemon  = document.getElementById("type_Pokemon");
-    selectTypePokemon.addEventListener("change", () =>{
-    let valueTypePokemon = document.getElementById("type_Pokemon").value    
-  //let valueCandy = selectCandy.options[selectCandy.selectedIndex].value
-   let printPokemon = window.filterData.thirdFilterType(data, valueTypePokemon)
-   
-  //                  // imprimir resultados Type
-  document.getElementById("showPokemones").innerHTML="";
-
-   printPokemon.forEach(element => {
-    container.innerHTML += `<div>
-                                  <div class="cartPk">
-                                  <div class="cartPk1">
-                                  <img src = ${element.img} class="imgPk"> 
-                           
-                                  <p> ${element.name}</p>
-                                  <p> # ${element.id}</p>
-                                 </div> 
-                                  </div>   
-                                  </div>  
-                              `
-      
-      })})
-
-      
+                          </div>
+                        </div>`
+    });
+  });
 
 
+  const selectEgg  = document.getElementById("type_egg");
+      selectEgg.addEventListener("change", () =>{
+      let valueEgg = document.getElementById("type_egg").value
+    //let valueCandy = selectCandy.options[selectCandy.selectedIndex].value
+     let prindEgg = window.filterData.secondFilterEggs(data, valueEgg)
+
+
+    //                  // imprimir resultados egss
+    document.getElementById("showPokemones").innerHTML="";
+    prindEgg.forEach(element => {
+      container.innerHTML += `<div>
+                                    <div class="cartPk">
+                                    <img src = ${element.img} class="imgPk">
+                                    <div class="cartPk1">
+                                    <p id="pokeName"> ${element.name}</p>
+                                    <p id="pokedex"> # ${element.id}</p>
+                                   </div>
+                                    </div>
+                                    </div>
+                                `
+
+        })})
+
+      const selectTypePokemon  = document.getElementById("type_Pokemon");
+      selectTypePokemon.addEventListener("change", () =>{
+      let valueTypePokemon = document.getElementById("type_Pokemon").value
+    //let valueCandy = selectCandy.options[selectCandy.selectedIndex].value
+     let printPokemon = window.filterData.thirdFilterType(data, valueTypePokemon)
+
+    //                  // imprimir resultados Type
+    document.getElementById("showPokemones").innerHTML="";
+
+     printPokemon.forEach(element => {
+      container.innerHTML += `<div>
+                                    <div class="cartPk">
+                                    <img src = ${element.img} class="imgPk">
+                                    <div class="cartPk1">
+                                    <p id="pokeName"> ${element.name}</p>
+                                    <p id="pokedex"> # ${element.id}</p>
+                                   </div>
+                                    </div>
+                                    </div>
+                                `
+
+        })})
